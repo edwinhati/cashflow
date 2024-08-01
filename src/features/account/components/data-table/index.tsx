f4b2a47b62db9ac59b514b4046a958da496a6fa1
edwinhati <edwinhati@outlook.com>
2024-08-02 00:39:28 +0700

    fix: Filter bug in accounts data table

--- src/features/account/components/data-table/index.tsx
+++ src/features/account/components/data-table/index.tsx
@@ -63,9 +63,9 @@ export function DataTable<TData, TValue>({
       <div className="flex items-center py-4">
           <Input
             placeholder="Filter accounts..."
-            value={(table.getColumn("account")?.getFilterValue() as string) ?? ""}
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
             onChange={(event) =>
-              table.getColumn("account")?.setFilterValue(event.target.value)
+              table.getColumn("name")?.setFilterValue(event.target.value)
             }
             className="max-w-sm"
           />
